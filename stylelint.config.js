e427f62e7270c0fd26d0210ccd5dab1de836a75e
wujieli0207 <lwj0207@foxmail.com>
2022-01-22 12:13:59 +0800

    build: 更新 stylelint 规则

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,13 +1,30 @@
+// module.exports = {
+//   root: true,
+//   plugins: ["stylelint-order"],
+//   customSyntax: "postcss-html",
+//   extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+//   ignoreFiles: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts"],
+//   overrides: [
+//     {
+//       files: ["*.vue", "**/*.vue", "*.html", "**/*.html"],
+//       extends: ["stylelint-config-recommended", "stylelint-config-html"],
+//     },
+//   ],
+// };
+
 module.exports = {
   root: true,
-  plugins: ["stylelint-order"],
-  customSyntax: "postcss-html",
-  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
-  ignoreFiles: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts"],
-  overrides: [
-    {
-      files: ["*.vue", "**/*.vue", "*.html", "**/*.html"],
-      extends: ["stylelint-config-recommended", "stylelint-config-html"],
-    },
+  plugins: ["stylelint-less", "stylelint-order"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "stylelint-config-prettier",
   ],
+  rules: {
+    // 禁止低优先级的选择器出现在高优先级的选择器之后
+    "no-descending-specificity": null,
+    // 禁止使用未知的伪元素选择器
+    "selector-pseudo-element-no-unknown": null,
+  },
+  ignoreFiles: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts"],
 };
